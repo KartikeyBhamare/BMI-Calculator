5063aab6aabaae371ba0bc5bd425da9d2064ca6a
Kartikey Bhamare <98818952+KartikeyBhamare@users.noreply.github.com>
2024-06-21 18:27:26 +0530

    Added calculate button to invoke calculator functionality

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomBar extends StatelessWidget {
+  BottomBar({super.key, required this.onTap, required this.buttonTitle});
+  final Function onTap;
+  final String buttonTitle;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap();
+      },
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+
+          ),
+        ),
+        color: kBottomContainerColour,
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        margin: EdgeInsets.only(top: 10),
+        
+      ),
+    );
+  }
+}
